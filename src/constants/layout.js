db9f2af1fb6c62c6cdf260da4290f49876736e32
Mezes131 <ellipsecorp35@gmail.com>
2025-11-26 11:25:46 +0100

    feat: update card dimensions

--- src/constants/layout.js
+++ src/constants/layout.js
@@ -1,5 +1,5 @@
-export const CARD_WIDTH = 2.75;
-export const CARD_HEIGHT = 3.15;
+export const CARD_WIDTH = 2.85;
+export const CARD_HEIGHT = 3;
 export const CARD_DEPTH = 0.1;
 export const CARD_HALF_WIDTH = CARD_WIDTH / 2;
 export const CARD_HALF_HEIGHT = CARD_HEIGHT / 2;
